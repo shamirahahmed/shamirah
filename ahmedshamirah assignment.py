bec20209b9d391ad6813d076ac3e527227c598d1
shamirahahmed <141908439+shamirahahmed@users.noreply.github.com>
2023-09-13 14:22:30 +0300

    Add files via upload

---
+++ ahmedshamirah assignment.py
@@ -0,0 +1,49 @@
+#even numbers 2,4,6
+n= 2,4,6
+print ("the sum of even numbers less than 8 is" + str (sum(n)))
+#12
+
+#Write a program in python to input 5 numbers from the keyboard and find their average
+#totalAverage of different countries
+Canada=3
+America=6
+Sudan=4
+Sweden=7
+Miami=9
+
+
+total_average_of_different_countries=(Canada +America +Sudan + Sweden+Miami)
+average= (total_average_of_different_countries)/5
+print(average)
+print("the average  of different countries is" + str(average))
+#5.8
+
+#write a python program to printout the following operations
+#a, -5+8*6
+a=-5
+b=8
+c=6
+m=b*c
+print (a+m)
+#43
+
+#modulus operators
+#b,55+9%9
+q=55
+z=9
+s=9
+v=(q+z)
+print(v%s)
+#1
+
+#c, 20+-3*5/8
+x=-3*(5/8)
+print(20 + x)
+#18.125
+
+#d,5+15/3*2-8%3
+g=(5+(15/3)*2)-8
+v=3
+print(g%v)
+#1.0
+
